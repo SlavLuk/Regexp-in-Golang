d3dd1324032a2f7557cef74d84c00ff45bdbfc88
g00339839 <g00339839@gmit.ie>
2018-04-06 16:52:34 +0100

    Added special char

--- regexpByGo.go
+++ regexpByGo.go
@@ -3,21 +3,40 @@ package main
 
 import (
 
-
 	"fmt"	
 )
 
 func intopost(infix string)string{
 
-	postfix:=""
+	specialschar:=map[rune]int{'*':10,'.':9,'|':8}
 
-	return postfix
+	pofix:=[]rune{}
+
+	s:=[]rune{}
+
+
+
+
+
+	return string(pofix)
 }
 
 func main() {
 
-	//Should print: ab.c*
+	//Should print: ab.c*.
 	fmt.Println("Infix: ","a.b.c*")
-	fmt.Println("Postfix: ",intopost("a.b.c*"))
+	fmt.Println("Pofix: ",intopost("a.b.c*"))
+
+	//Should print: abd|.*
+	fmt.Println("Infix: ","(a.(b|d))*")
+	fmt.Println("Pofix: ",intopost("(a.(b|d))*"))
+
+	//Should print: abd|.c*.
+	fmt.Println("Infix: ","a.(b|d).c*")
+	fmt.Println("Pofix: ",intopost("a.(b|d).c*"))
+
+	//Should print: abb.+.c.
+	fmt.Println("Infix: ","a.(b.b)+.c")
+	fmt.Println("Pofix: ",intopost("a.(b.b)+.c"))
 
 }
